3b8d60f72899192bd8c1cb59f0503f619d62e51f
unknown <micaelpedrosa@gmail.com>
2016-05-09 15:11:32 +0100

    First view structure

--- rethink/src/app.ts
+++ rethink/src/app.ts
@@ -2,21 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'div[my-app]',
-  template: `
-    <button (click)="onClick()">
-      <i class="home icon"></i>
-      {{text}}
-    </button>
-  `
+  templateUrl: 'view/app.html'
 })
 export class AppComponent {
-  text = "Follow"
 
-  ngOnInit() {
-    console.log("init");
-  }
-
-  onClick() {
-    this.text = "Following"
-  }
 }
