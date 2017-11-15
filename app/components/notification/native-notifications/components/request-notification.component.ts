e921a5c10825d7bc2b1a82d9d2887a51f23bee4c
luistduarte <lduarte.suil@gmail.com>
2017-11-15 17:32:32 +0000

    update app

---
+++ app/components/notification/native-notifications/components/request-notification.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'request-notification',
+  templateUrl: './request-notification.component.html',
+  styleUrls: ['./request-notification.component.scss']
+})
+
+export class RequestNotificationComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
