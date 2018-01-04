c012d77e427c41e99234983fa3b97336a38d9e81
Vitor Silva <vitor-t-silva@telecom.pt>
2018-01-04 15:21:29 +0000

    Update the environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  host: 'localhost',
+  host: 'hysmart.rethink.ptinovacao.pt',
   baseHref: '/',
   images: './assets/img',
   sounds: './assets/sounds'
